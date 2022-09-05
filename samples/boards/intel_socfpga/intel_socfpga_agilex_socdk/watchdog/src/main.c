6cd8b993994635a2e0478aede43dcaa176a454be
Amit Raddi <amit.raddi@intel.com>
2022-09-05 09:52:23 +0000

    samples: intel_socfpga: Watchdog: Updated to test interrupt mode support
    Updated watchdog sample application to test interrupt mode on two
    watchdog instances
    
    Signed-off-by: Amit Raddi <amit.raddi@intel.com>

--- samples/boards/intel_socfpga/intel_socfpga_agilex_socdk/watchdog/src/main.c
+++ samples/boards/intel_socfpga/intel_socfpga_agilex_socdk/watchdog/src/main.c
@@ -14,10 +14,14 @@
 
 #if DT_HAS_COMPAT_STATUS_OKAY(snps_designware_watchdog)
 
-#define WDT_COUNT	4
-#define WDT_FEED_TRIES	20000
-#define WDT_FEED_PRINT	1000
-#define DELAY_COUNT	10000
+#define WDT_COUNT		4
+#define WDT_FEED_TRIES	6
+#define DELAY_MS		500
+
+#define WDT_0_TIMEOUT_MS	1000
+#define WDT_1_TIMEOUT_MS	2000
+#define WDT_2_TIMEOUT_MS	5000
+#define WDT_3_TIMEOUT_MS	10000
 
 #define WDT_NODE_0 DT_INST(0, snps_designware_watchdog)
 #define WDT_NODE_1 DT_INST(1, snps_designware_watchdog)
@@ -29,18 +33,49 @@
 #define WDT_DEV_NAME_2 DT_LABEL(WDT_NODE_2)
 #define WDT_DEV_NAME_3 DT_LABEL(WDT_NODE_3)
 
-static char *wdt_dev_name[WDT_COUNT] = {
-	WDT_DEV_NAME_0,
-	WDT_DEV_NAME_1,
-	WDT_DEV_NAME_2,
-	WDT_DEV_NAME_3
+struct wdt_configuration {
+	char *wdt_dev_name;
+	uint32_t timeout_ms;
+	bool interrupt_enabled;
 };
 
-static int wdt_inst_setup(const struct device *wdt, int timeout_ms);
+static struct wdt_configuration wdt_config[WDT_COUNT] = {
+	{
+		.wdt_dev_name = WDT_DEV_NAME_0,
+		.timeout_ms = WDT_0_TIMEOUT_MS,
+		.interrupt_enabled = (bool)DT_IRQ_HAS_CELL(WDT_NODE_0, irq),
+	},
+	{
+		.wdt_dev_name = WDT_DEV_NAME_1,
+		.timeout_ms = WDT_1_TIMEOUT_MS,
+		.interrupt_enabled = (bool)DT_IRQ_HAS_CELL(WDT_NODE_1, irq),
+	},
+	{
+		.wdt_dev_name = WDT_DEV_NAME_2,
+		.timeout_ms = WDT_2_TIMEOUT_MS,
+		.interrupt_enabled = (bool)DT_IRQ_HAS_CELL(WDT_NODE_2, irq),
+	},
+	{
+		.wdt_dev_name = WDT_DEV_NAME_3,
+		.timeout_ms = WDT_3_TIMEOUT_MS,
+		.interrupt_enabled = (bool)DT_IRQ_HAS_CELL(WDT_NODE_3, irq),
+	},
+};
+
+static int wdt_inst_timeout(const struct device *wdt, uint32_t timeout_ms, bool interrupt_enabled);
+
+static void wdt_callback(const struct device *wdt_dev, int channel_id)
+{
+	printk("Interrupt triggered for %s\n", wdt_dev->name);
+
+	printk("Feeding watchdog %s...\n", wdt_dev->name);
+
+	wdt_feed(wdt_dev, channel_id);
+}
 
 int main(void)
 {
-	int i, j, count;
+	int i, j;
 	int ret = 0;
 	const struct device *wdt[WDT_COUNT];
 
@@ -48,41 +83,54 @@ int main(void)
 
 	/* Enable all 4 watchdogs */
 	for (i = 0; i < WDT_COUNT; ++i) {
-		wdt[i] = device_get_binding(wdt_dev_name[i]);
-		ret = wdt_inst_setup(wdt[i], WDT_FEED_TRIES);
+		wdt[i] = device_get_binding(wdt_config[i].wdt_dev_name);
+
+		ret = wdt_inst_timeout(wdt[i], wdt_config[i].timeout_ms,
+					wdt_config[i].interrupt_enabled);
+
 		if (ret != 0) {
-			printk("Watchdog %d setup failed.\n", i);
+			printk("Watchdog %s install timeout error [%d]\n", wdt[i]->name, ret);
 			return ret;
 		}
 
-		printk("Watchdog %d setup successful.\n", i);
+		ret = wdt_setup(wdt[i], WDT_OPT_PAUSE_HALTED_BY_DBG);
+
+		if (ret != 0) {
+			printk("Watchdog %s setup error [%d]", wdt[i]->name, ret);
+			return ret;
+		}
+
+		printk("Watchdog %s setup successful.\n", wdt[i]->name);
 	}
 
 	/* Test watchdog counter restart */
-	printk("Feeding watchdog %d times\n", WDT_FEED_TRIES);
+	printk("Feeding watchdogs configured for Reset Mode %d times\n", WDT_FEED_TRIES);
+
 	for (j = 0; j < WDT_FEED_TRIES; ++j) {
-		if ((j % WDT_FEED_PRINT) == 0) {
-			printk("Feeding watchdog...\n");
-		}
+		printk("Feeding watchdogs...\n");
 
 		for (i = 0; i < WDT_COUNT; ++i) {
-			wdt_feed(wdt[i], 0);
+			if (wdt_config[i].interrupt_enabled == false) {
+				wdt_feed(wdt[i], 0);
+			}
 		}
 
-		/* Use while loop to mimic a delay */
-		count = DELAY_COUNT;
-		while (count >= 0) {
-			count--;
-		}
+		k_msleep(DELAY_MS);
 	}
 
-	/* Test Done */
 	printk("Watchdog test sample end\n");
+
+	/* Waiting for the SoC reset. */
+	printk("Waiting for reset...\n");
+
+	while (true) {
+		k_yield();
+	}
 
 	return 0;
 }
 
-int wdt_inst_setup(const struct device *wdt, int timeout_ms)
+static int wdt_inst_timeout(const struct device *wdt, uint32_t timeout_ms, bool interrupt_enabled)
 {
 	int ret = 0;
 
@@ -91,28 +139,24 @@ int wdt_inst_setup(const struct device *wdt, int timeout_ms)
 		return -ENODEV;
 	}
 
-	struct wdt_timeout_cfg wdt_config = {
+	struct wdt_timeout_cfg wdt_timeout_config = {
 		/* Reset SoC when watchdog timer expires. */
 		.flags = WDT_FLAG_RESET_SOC,
 
 		/* Expire watchdog after max window */
 		.window.min = 0U,
 		.window.max = timeout_ms,
+
+		.callback = NULL,
 	};
 
-	ret = wdt_install_timeout(wdt, &wdt_config);
-	if (ret != 0) {
-		printk("Watchdog install timeout error");
-		return ret;
+	if (interrupt_enabled == true) {
+		wdt_timeout_config.callback = wdt_callback;
 	}
 
-	ret = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
-	if (ret != 0) {
-		printk("Watchdog setup error");
-		return ret;
-	}
+	ret = wdt_install_timeout(wdt, &wdt_timeout_config);
 
-	return 0;
+	return ret;
 }
 
 #endif
