fc89f04784eff6fda695a2b4004c76291f34f17f
Chirag Kochar <chirag.kochar@intel.com>
2023-04-19 16:25:01 +0800

    Revert "drivers: gpio: Exclusion from steps if the driver has no data struct"
    This reverts commit 8ac4b566fe54a939c47d56308aaa05593725ddf4.
    This change was made to handle gpio driver without driver data
    structure.
    
    Signed-off-by: Chirag Kochar <chirag.kochar@intel.com>

--- include/zephyr/drivers/gpio.h
+++ include/zephyr/drivers/gpio.h
@@ -723,12 +723,10 @@ static inline int z_impl_gpio_pin_configure(const struct device *port,
 	__ASSERT((cfg->port_pin_mask & (gpio_port_pins_t)BIT(pin)) != 0U,
 		 "Unsupported pin");
 
-	if (data) {
-		if ((flags & GPIO_ACTIVE_LOW) != 0) {
-			data->invert |= (gpio_port_pins_t)BIT(pin);
-		} else {
-			data->invert &= ~(gpio_port_pins_t)BIT(pin);
-		}
+	if ((flags & GPIO_ACTIVE_LOW) != 0) {
+		data->invert |= (gpio_port_pins_t)BIT(pin);
+	} else {
+		data->invert &= ~(gpio_port_pins_t)BIT(pin);
 	}
 
 	return api->pin_configure(port, pin, flags);
@@ -985,7 +983,7 @@ static inline int gpio_port_get(const struct device *port,
 	int ret;
 
 	ret = gpio_port_get_raw(port, value);
-	if (ret == 0 && data) {
+	if (ret == 0) {
 		*value ^= data->invert;
 	}
 
@@ -1344,7 +1342,7 @@ static inline int gpio_pin_set(const struct device *port, gpio_pin_t pin,
 	__ASSERT((cfg->port_pin_mask & (gpio_port_pins_t)BIT(pin)) != 0U,
 		 "Unsupported pin");
 
-	if (data && data->invert & (gpio_port_pins_t)BIT(pin)) {
+	if (data->invert & (gpio_port_pins_t)BIT(pin)) {
 		value = (value != 0) ? 0 : 1;
 	}
 
